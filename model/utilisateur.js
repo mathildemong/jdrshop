772f7f4ecdf24c435df0e6556da682e9bf7a4ad1
mathildemong <mmonguillon@gmail.com>
2023-11-24 13:08:34 +0100

    Add files via upload

---
+++ model/utilisateur.js
@@ -0,0 +1,10 @@
+class Utilisateur{
+    constructor(nom, adresse, telephone, mail){
+        this.username = nom;
+        this.adress = adresse;
+        this.phone = telephone;
+        this.mail = mail;
+    }
+}
+
+module.exports = {Utilisateur};
