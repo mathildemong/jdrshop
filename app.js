6e5ff9794011583fcf16bf6a55dcbba16d8730c8
mathildemong <mmonguillon@gmail.com>
2024-01-16 14:25:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
 const express = require('express');
 const { connect } = require('./bd/connect');
-const routesUtilisateur = require("./route/utilisateur");
+// Routes for router
+
+const userRoutes = require('./routes/user')
+const productRoutes = require('./routes/product')
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-appUse("/api/v1", routesUtilisateur);
+app.use("/api/v1", userRoutes)
+app.use("/api/v1", productRoutes)
 
-connect( "mongodb://127.0.0.1:27017/", (erreur) => {   // mongodb+srv://mmonguillon:mercure10@jdrshop.vpc0shq.mongodb.net/ ?? 
+// Please replace the following url with what you use :) 
+connect('mongodb+srv://mmonguillon:mercure10@jdrshop.vpc0shq.mongodb.net', (erreur) => {  
     if(erreur) {
     console.log("erreur a la conection a la base de donnee");
     process.exit(-1);
