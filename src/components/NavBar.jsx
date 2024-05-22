d77ce22926ec581bc7edc3ee2b04230fbd65349f
mathildemong <mmonguillon@gmail.com>
2024-05-22 19:34:32 +0200

    tri dans code avec gab
    suite a mauvais commit de ma part

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
       <div className="leftSide" id={openLinks ? "open" : "close"}>
        <div>  <Link to="/"><img src={Logo} alt="logo"/></Link></div>
         <div className="hiddenLinks">
-    <Link to="/">Home </Link>
+    <Link to="/"> Home </Link>
         <Link to="/Shop"> Shop </Link>
         <Link to="/login"> Login </Link>
       <Link to="/cart">
