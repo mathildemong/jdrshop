1eec2a2b8b9fdfdbe8474857a720b43f9fa11e59
Romain Bontemps <romain.bontemps@payfit.com>
2024-06-17 12:51:58 +0200

    fix: revert back to using mongo on demand

--- BACK/app.js
+++ BACK/app.js
@@ -14,22 +14,8 @@ app.use("/api/v1", userRoutes);
 app.use("/api/v1", productRoutes);
 
 
-// connect(
-//   "mongodb+srv://mmonguillon:mercure10@jdrshop.vpc0shq.mongodb.net",
-//   (erreur) => {
-//     if (erreur) {
-//       console.log("erreur a la conection a la base de donnee");
-//       process.exit(-1);
-//     } else {
-//       console.log("connection avec la base de donnee etablie");
-//       app.listen(3000);
-//       console.log("attente des requetes du port 3000");
-//     }
-//   }
-// );
-
 connect(
-  "mongodb://root:example@localhost:27017",
+  "mongodb+srv://mmonguillon:mercure10@jdrshop.vpc0shq.mongodb.net",
   (erreur) => {
     if (erreur) {
       console.log("erreur a la conection a la base de donnee");
@@ -41,6 +27,21 @@ connect(
     }
   }
 );
+
+// Local Test env
+// connect(
+//   "mongodb://root:example@localhost:27017",
+//   (erreur) => {
+//     if (erreur) {
+//       console.log("erreur a la conection a la base de donnee");
+//       process.exit(-1);
+//     } else {
+//       console.log("connection avec la base de donnee etablie");
+//       app.listen(3000);
+//       console.log("attente des requetes du port 3000");
+//     }
+//   }
+// );
 
 // const express = require('express');
 // const app = express();
